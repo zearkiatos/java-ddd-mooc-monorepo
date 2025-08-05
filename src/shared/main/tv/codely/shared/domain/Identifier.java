b4ca41cfdd6ab1f92739e6650ac94ab23d49b507
zearkiatos <caprilespe@outlook.com>
2025-08-04 22:17:35 -0400

    UUIDs as identifier

---
+++ src/shared/main/tv/codely/shared/domain/Identifier.java
@@ -0,0 +1,39 @@
+package tv.codely.shared.domain;
+
+import java.util.UUID;
+
+public abstract class Identifier {
+    private UUID value;
+
+    public Identifier(String value) {
+        ensureValidUuid(value);
+        this.value = UUID.fromString(value);
+    }
+
+    private void ensureValidUuid(String value) {
+        UUID.fromString(value);
+    }
+
+    public String value() {
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Identifier that = (Identifier) o;
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
