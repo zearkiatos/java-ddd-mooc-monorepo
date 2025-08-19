7bd3886aa14ad2a7ff63ec48cf0075389a1c9f21
zearkiatos <caprilespe@outlook.com>
2025-08-19 19:39:33 -0400

    Adding a dataProvider

--- src/mooc/test/tv/codely/mooc/steps/infrastructure/MySqlStepRepositoryShould.java
+++ src/mooc/test/tv/codely/mooc/steps/infrastructure/MySqlStepRepositoryShould.java
@@ -7,9 +7,13 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.Arrays;
+import java.util.List;
+
 import tv.codely.mooc.steps.StepsModuleInfrastructureTestCase;
 import tv.codely.mooc.steps.domain.video.VideoStep;
 import tv.codely.mooc.steps.domain.video.VideoStepMother;
+import tv.codely.mooc.steps.domain.Step;
 import tv.codely.mooc.steps.domain.challenge.ChallengeStep;
 import tv.codely.mooc.steps.domain.challenge.ChallengeStepMother;
 
@@ -17,43 +21,33 @@ import tv.codely.mooc.steps.domain.challenge.ChallengeStepMother;
 @ActiveProfiles("local")
 public class MySqlStepRepositoryShould extends StepsModuleInfrastructureTestCase {
     @Test
-    void save_a_video_step() {
+    void save_a_step() {
+        for (Step step : steps()) {
+            mySqlStepRepository.save(step);
+        }
         VideoStep step = VideoStepMother.random();
-
-        mySqlStepRepository.save(step);
     }
 
     @Test
-    void save_a_challenge_step() {
-        ChallengeStep step = ChallengeStepMother.random();
+    void return_an_exsisting_step() {
+        for (Step step : steps()) {
+            mySqlStepRepository.save(step);
 
-        mySqlStepRepository.save(step);
-    }
-
-    @Test
-    void return_an_exsisting_challenge_step() {
-        ChallengeStep step = ChallengeStepMother.random();
-
-        mySqlStepRepository.save(step);
-
-        assertEquals(step, mySqlStepRepository.search(step.id()).get());
-    }
-
-    @Test
-    void return_an_existing_video_step() {
-        VideoStep step = VideoStepMother.random();
-
-        mySqlStepRepository.save(step);
-
-        assertEquals(step, mySqlStepRepository.search(step.id()).get());
+            assertEquals(step, mySqlStepRepository.search(step.id()).get());
+        }
     }
 
     @Test
     void not_return_a_non_existing_step() {
-        assertFalse(mySqlStepRepository.search(ChallengeStepMother.random().id()).isPresent());
-        assertFalse(mySqlStepRepository.search(VideoStepMother.random().id()).isPresent());
+        for (Step step : steps()) {
+            assertFalse(mySqlStepRepository.search(step.id()).isPresent());
+        }
     }
 
-
+    private List<? extends Step> steps() {
+        return Arrays.asList(
+                VideoStepMother.random(),
+                ChallengeStepMother.random());
+    }
 
 }
