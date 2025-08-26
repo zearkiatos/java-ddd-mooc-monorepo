9769d5c17bb04311f17afc32e07dbcbfdefc424b
zearkiatos <caprilespe@outlook.com>
2025-08-25 23:19:04 -0400

    Adding springApplication event bus

---
+++ src/shared/main/tv/codely/shared/infrastructure/bus/event/SpringApplicationEventBus.java
@@ -0,0 +1,28 @@
+package tv.codely.shared.infrastructure.bus.event;
+
+import org.springframework.context.ApplicationEventPublisher;
+import java.util.List;
+
+import tv.codely.shared.domain.bus.event.DomainEvent;
+import tv.codely.shared.domain.bus.event.EventBus;
+
+import tv.codely.shared.domain.ServiceInjectable;
+
+@ServiceInjectable
+public class SpringApplicationEventBus implements EventBus {
+    private final ApplicationEventPublisher publisher;
+
+    public SpringApplicationEventBus(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(final DomainEvent<?> events) {
+        events.forEach(this::publish);
+    }
+
+    @Override
+    public void publish(final DomainEvent<?> event) {
+        this.publisher.publishEvent(event);
+    }
+}
