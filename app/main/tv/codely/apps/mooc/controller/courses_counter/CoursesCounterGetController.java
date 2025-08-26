62bd11c027c42914b9a6d083b0c2ed9457e029b1
zearkiatos <caprilespe@outlook.com>
2025-08-25 23:42:59 -0400

    [WIP] imcrement course counter

---
+++ app/main/tv/codely/apps/mooc/controller/courses_counter/CoursesCounterGetController.java
@@ -0,0 +1,27 @@
+package tv.codely.apps.mooc.controller.courses_counter;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public final class CoursesCounterGetController {
+    CoursesCounterFinder finder;
+
+    public CoursesCounterGetController(CoursesCounterFinder finder) {
+        this.finder = finder;
+    }
+
+    @GetMapping("/courses-counter")
+    public HashMap<String, Integer> index() {
+        CoursesCounterResponse response = finder.find();
+        return new HashMap<String,Integer>() {{
+            put("total", response.total());
+        }};
+    }
+
+
+
+}
---
+++ app/test/tv/codely/apps/mooc/controller/courses_counter/CoursesCounterGetControllerShould.java
@@ -0,0 +1,31 @@
+package tv.codely.apps.mooc.controller.courses_counter;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+
+import tv.codely.mooc.courses.domain.CourseCreatedDomainEvent;
+import tv.codely.shared.infrastructure.ApplicationTestCase;
+
+public final class CoursesCounterGetControllerShould extends ApplicationTestCase {
+    @Test
+    void get_the_counter_with_one_course() {
+        givenISendEventsToTheBus(
+            new CourseCreatedDomainEvent("8f34bc99-e0e2-4b2c-9f1f-1c4b2c9f1f4b", "DDD in Java", "7 days")
+        );
+
+        assertResponse("/courses-counter", 200, "{'total':1}");
+    }
+
+    @Test
+    void get_the_counter_with_more_than_one_course() throws Exception {
+        givenISendEventsToTheBus(
+            new CourseCreatedDomainEvent("8f34bc99-e0e2-4b2c-9f1f-1c4b2c9f1f4b", "DDD in Java", "7 days"),
+            new CourseCreatedDomainEvent("8f34bc99-e0e2-4b2c-9f1f-1c4b2c9f1f4c", "DDD in JavaScript", "7 days")
+        );
+
+        assertResponse("/courses-counter", 200, "{'total':2}");
+    }
+
+
+}
