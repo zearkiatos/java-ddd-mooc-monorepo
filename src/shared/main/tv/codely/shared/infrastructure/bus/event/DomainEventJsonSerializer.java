180eacc228f62630813a8cf7842eab1ed6cce6dc
zearkiatos <caprilespe@outlook.com>
2025-09-14 17:15:06 -0300

    Public events in RabbitMQ

---
+++ src/shared/main/tv/codely/shared/infrastructure/bus/event/DomainEventJsonSerializer.java
@@ -0,0 +1,25 @@
+package tv.codely.shared.infrastructure.bus.event;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import tv.codely.shared.domain.Utils;
+import tv.codely.shared.domain.bus.event.DomainEvent;
+
+public final class DomainEventJsonSerializer {
+    public static String serializer(DomainEvent<?> domainEvent) {
+        HashMap<String, Serializable> attributes = domainEvent.toPrimitives();
+        attributes.put("id", domainEvent.aggregateId());
+
+        return Utils.jsonEncode(new HashMap<String, Serializable>(){{
+            put("data", new HashMap<String, Serializable>() {{
+                put("id", domainEvent.id());
+                put("type", domainEvent.eventName());
+                put("occurred_on", domainEvent.occurredOn());
+                put("attributes", attributes);
+            }});
+            put("meta", new HashMap<String, Serializable>());
+        }});
+
+    }
+}
---
+++ src/shared/main/tv/codely/shared/infrastructure/bus/event/rabbitmq/RabbitMqEventBus.java
@@ -0,0 +1,34 @@
+package tv.codely.shared.infrastructure.bus.event.rabbitmq;
+
+import java.util.List;
+
+import org.springframework.amqp.AmqpException;
+import tv.codely.domain.ServiceInjectable;
+import tv.codely.shared.domain.bus.event.DomainEvent;
+import tv.codely.shared.domain.bus.event.EventBus;
+
+
+@ServiceInjectable
+public class RabbitMqEventBus implements EventBus {
+    private final RabbitMqPublisher publisher;
+    private final String exchangeName;
+
+    public RabbitMqEventBus(RabbitMqPublisher publisher) {
+        this.publisher = publisher;
+        this.exchangeName = "domain_events";
+    }
+
+    @Override
+    public void publish(List<DomainEvent<?>> events) {
+        events.forEach(this::publish);
+    }
+
+    @Override
+    public void publish(DomainEvent<?> domainEvent) {
+        try {
+            publisher.publish(domainEvent, exchangeName);
+        } catch(AmqpException error) {
+            // KAPACHAO
+        }
+    }
+}
