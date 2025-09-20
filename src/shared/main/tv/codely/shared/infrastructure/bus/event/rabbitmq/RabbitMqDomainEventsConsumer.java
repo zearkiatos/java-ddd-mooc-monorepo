feeec5d5672008034f46f1104c41484f02ff11f1
zearkiatos <caprilespe@outlook.com>
2025-09-19 22:45:56 -0300

    Handle errors when consume with RabbitMQ

--- src/shared/main/tv/codely/shared/infrastructure/bus/event/rabbitmq/RabbitMqDomainEventsConsumer.java
+++ src/shared/main/tv/codely/shared/infrastructure/bus/event/rabbitmq/RabbitMqDomainEventsConsumer.java
@@ -3,12 +3,15 @@ package tv.codely.shared.infrastructure.bus.event.rabbitmq;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.listener.AbstractMessageListenerContainer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.context.ApplicationContext;
+import org.springframework.amqp.core.MessagePropertiesBuilder;
+import org.springframework.amqp.core.MessageBuilder;
 
 import tv.codely.shared.infrastructure.bus.event.DomainEventJsonDeserializer;
 import tv.codely.shared.infrastructure.bus.event.rabbitmq.RabbitMqPublisher;
@@ -23,6 +26,7 @@ public final class RabbitMqDomainEventsConsumer {
     private final DomainEventJsonDeserializer   deserializer;
     private final RabbitMqPublisher publisher;
     private final HashMap<String, Object> domainEventsSubscribers = new HashMap<>();
+    private final int MAX_RETRIES = 2;
     RabbitListenerEndpointRegistry registry;
     DomainSubscribersInformation information;
 
@@ -67,18 +71,53 @@ public final class RabbitMqDomainEventsConsumer {
             throw new Exception(String.format("The subscriber <%s> should implement the method 'on' listening the domain event <%s>", queue, domainEvent.eventName()));
         }
         catch(Exception error) {
-            // KAPACHAO!!!
+            handleConsumptionError(message, queue);
         }
 
         System.out.println("asd");
         System.out.println();
     }
 
-     public void withSubscribersInformation(DomainEventSubscribersInformation information) {
+    private void handleConsumptionError(Message message, String queue) {
+        if (hasBeenRedeliveredTooMuch(message)) {
+            sendToDeadLetter(message, queue);
+        }
+        else {
+            sendToRetry(message, queue);
+        }
+    }
+
+    private boolean hasBeenRedeliveredTooMuch(Message message) {
+        return (int)message.getMessageProperties().getHeaders.getOrDefault("redelivery_count", 0) >= MAX_RETRIES;
+    }
+
+    private void sendToRetry(Message message, String queue) {
+        sendMessageTo(RabbitMqExchangeNameFormatter.retry("domain_events"), queue, message);
+    }
+
+    private void sendToDeadLetter(Message message, String queue) {
+        sendMessageTo(RabbitMqExchangeNameFormatter.deadLetter("domain_events"), queue, message);
+    }
+
+    private void sendMessageTo(String exchange, Message message, String queue) {
+        Map<String, Object> headers = message.getMessageProperties().getHeaders();
+        headers.put("redelivery_count", (int)headers.getOrDefault("redelivery_count", 0) + 1);
+
+        MessageBuilder.fromMessage(message).andProperties(
+            MessagePropertiesBuilder.newInstance()
+            .setContentEncoding("utf-8")
+            .setContentType("application/json")
+            .copyHeaders(headers)
+            .build()
+        );
+        publisher.publish(message, exchange, queue);
+    }
+
+     public void withSubscribersInformation(DomainEventSubscribersInformation information) {|
         this.information = information;
     }
 
-     private Object subscriberFor(String queue) {
+     private Object subscriberFor(String queue) throws Exception {
         String[] queueParts     = queue.split("\\.");
         String   subscriberName = Utils.toCamelFirstLower(queueParts[queueParts.length - 1]);
 
@@ -88,8 +127,7 @@ public final class RabbitMqDomainEventsConsumer {
 
             return subscriber;
         } catch (Exception e) {
-            System.out.println(String.join("\n", context.getBeanDefinitionNames()));
-            System.out.println(e.getMessage());
+            throw new Exception(String.format("There are not registered subscribers for <%s> queue", queue));
         }
 
         return null;
