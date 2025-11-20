99a5879a7878571b8749b79bb2c7fd938b65ce20
zearkiatos <caprilespe@outlook.com>
2025-11-19 22:39:39 -0300

    Adding infrastructure for video project in backoffice

---
+++ src/backoffice/main/tv/codely/backoffice/videos/infrastructure/MySqlBackofficeVideoRepository.java
@@ -0,0 +1,30 @@
+package tv.codely.backoffice.videos.infrastructure;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import tv.codely.backoffice.videos.domain.BackofficeVideo;
+import tv.codely.backoffice.videos.domain.BackofficeVideoRepository;
+import tv.codely.shared.infrastructure.hibernate.HibernateRepository;
+import tv.codely.shared.domain.ServiceInjectable;
+
+@ServiceInjectable
+@Transactional("backoffice-transaction_manager")
+public class MySqlBackofficeVideoRepository extends HibernateRepository<BackofficeVideo> implements BackofficeVideoRepository {
+    public MySqlBackofficeVideoRepository(@Qualifier("backoffice-session_factory") SessionFactory sessionFactory) {
+        super(sessionFactory, BackofficeVideo.class);
+    }
+
+    @Override
+    public void save(BackofficeVideo video) {
+        persist(video);
+    }
+
+    @Override
+    public List<BackofficeVideo> searchAll() {
+        return all();
+    }
+}
