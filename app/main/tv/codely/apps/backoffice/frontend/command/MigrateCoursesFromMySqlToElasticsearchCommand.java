b00bd108b18aad9fc0a3f7c2d5ba53643c57bf3f
zearkiatos <caprilespe@outlook.com>
2025-11-25 23:06:41 -0300

    Keeping sync the projection with elasticsearch

---
+++ app/main/tv/codely/apps/backoffice/frontend/command/MigrateCoursesFromMySqlToElasticsearchCommand.java
@@ -0,0 +1,24 @@
+package tv.codely.apps.backoffice.frontend.command;
+
+import tv.codely.backoffice.courses.infrastructure.ElasticsearchBackofficeCourseRepository;
+import tv.codely.backoffice.courses.infrastructure.MySqlBackofficeCourseRepository;
+import tv.codely.shared.infrastructure.elasticsearch.ElasticsearchRepository;
+
+public final class MigrateCoursesFromMySqlToElasticsearchCommand {
+    private final MySqlBackofficeCourseRepository mysqlRepository;
+    private final ElasticsearchBackofficeCourseRepository elasticsearchRepository;
+
+    public MigrateCoursesFromMySqlToElasticsearchCommand(
+            MySqlBackofficeCourseRepository mysqlRepository,
+            ElasticsearchBackofficeCourseRepository elasticsearchRepository
+    ) {
+        this.mysqlRepository = mysqlRepository;
+        this.elasticsearchRepository = elasticsearchRepository;
+    }
+
+    public void execute() {
+        mysqlRepository
+                .searchAll()
+                .forEach(elasticsearchRepository::save);
+    }
+}
---
+++ src/backoffice/main/tv/codely/backoffice/courses/infrastructure/ElasticsearchBackofficeCourseRepository.java
@@ -0,0 +1,42 @@
+package tv.codely.backoffice.courses.infrastructure;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+import tv.codely.shared.domain.criteria.Criteria;
+import tv.codely.shared.infrastructure.elasticsearch.ElasticsearchClient;
+import tv.codely.backoffice.courses.domain.BackofficeCourse;
+import tv.codely.backoffice.courses.domain.BackofficeCourseRepository;
+import tv.codely.shared.infrastructure.elasticsearch.ElasticsearchRepository;
+import tv.codely.shared.domain.ServiceInjectable;
+
+@Primary
+@ServiceInjectable
+public final class ElasticsearchBackofficeCourseRepository extends ElasticsearchRepository<BackofficeCourse>
+        implements BackofficeCourseRepository {
+    public ElasticsearchBackofficeCourseRepository(ElasticsearchClient client) {
+        super(client);
+    }
+
+    @Override
+    public void save(BackofficeCourse course) {
+        persist(course.id(), course.toPrimitives());
+    }
+
+    @Override
+    public List<BackofficeCourse> searchAll() {
+        return searchAllInElastic(BackofficeCourse::fromPrimitives);
+    }
+
+    @Override
+    public List<BackofficeCourse> matching(Criteria criteria) {
+        return searchAllInElastic(BackofficeCourse::fromPrimitives);
+    }
+
+    @Override
+    protected String moduleName() {
+        return "courses";
+    }
+
+
+}
